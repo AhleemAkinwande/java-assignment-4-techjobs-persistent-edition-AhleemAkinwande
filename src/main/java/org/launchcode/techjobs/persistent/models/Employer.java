fd5dd1feb4dd4fab9f59ae1d1c5cd71a2e835f0c
ahleem <72522827+AhleemAkinwande@users.noreply.github.com>
2022-10-10 21:04:15 -0400

    Test One: JPA dependencies created and working.
    Employer and Skill repo created. EmployerController updated, and SkillController created. Skill and Employer classes also modified.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Employer extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Entity
+public class Employer extends AbstractEntity{
 
+    @NotNull(message = "Name required!")
+    //@NotBlank(message = "Do not leave blank!")
+    @Size(min = 1, max = 255)
+    private String location;
+
+    public Employer() {};
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
 }
